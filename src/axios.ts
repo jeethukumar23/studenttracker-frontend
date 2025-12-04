a776726fd22dae79907ec7c85054c5dd5a809576
jeethukumar23 <2300031651@kluniversity.in>
2025-12-04 21:13:24 +0530

    Updated changes

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:9091/api",
+  baseURL: import.meta.env.VITE_API_URL, // 👈 using env variable
   headers: { "Content-Type": "application/json" },
 });
 
